e909bcaf92dc4828dd8ae042f562a4f57e0981fa
jotapesalles <joaopaulo_jpds@hotmail.com>
2020-11-26 08:22:02 -0300

    Validation exception

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,6 +3,7 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 import gui.listeners.DataChangeListener;
@@ -15,6 +16,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
@@ -48,10 +50,15 @@ public class DepartmentFormController implements Initializable {
 		if(service == null) {
 			throw new IllegalStateException("Service was null!");
 		}
-		entity = getFormData();
-		service.saveOrUpdate(entity);
-		notifyDataChangeListeners();
-		Utils.currentStage(event).close();
+		try {
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			notifyDataChangeListeners();
+			Utils.currentStage(event).close();
+		}
+		catch(ValidationException e) {
+			setErrorMessages(e.getErrors());
+		}
 	}
 	private void notifyDataChangeListeners() {
 		for(DataChangeListener l : dataChangeListeners) {
@@ -59,9 +66,15 @@ public class DepartmentFormController implements Initializable {
 		}
 	}
 	private Department getFormData() {
+		ValidationException exception = new ValidationException("Validation error.");
 		Department dep = new Department();
 		dep.setId(gui.util.Utils.tryParseToInt(txtID.getText()));
+		if(txtName.getText() == null || txtName.getText().trim() == null) {
+			exception.addError("Name", "Field can't be empty!");
+		}
 		dep.setName(txtName.getText());
+		if(exception.getErrors().size() > 0) throw exception;
+		
 		return dep;
 	}
 	@FXML
@@ -93,6 +106,10 @@ public class DepartmentFormController implements Initializable {
 		txtName.setText(entity.getName());
 	}
 	
+	private void setErrorMessages(Map<String, String> errors) {
+		if(errors.containsKey("Name"))
+			lblErrorName.setText(errors.get("Name"));
+	}
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		initializeNodes();
